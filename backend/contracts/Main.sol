9ac9eed594931a06a9a1df9154b50ae0e156503d
Rafał <rafal44334@gmail.com>
2023-07-03 23:52:21 +0200

    Smart contract: updates to the DAO logic

--- backend/contracts/Main.sol
+++ backend/contracts/Main.sol
@@ -5,11 +5,17 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract IzanagiDAO is ReentrancyGuard, AccessControl {
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     // Contract constants
     bytes32 public constant CONTRIBUTOR_ROLE = keccak256("CONTRIBUTOR"); // AccessControl bases on bytes32
     bytes32 public constant STAKEHOLDER_ROLE = keccak256("STAKEHOLDER");
     uint32 constant minimumVotingPeriod = 1 weeks;
     uint256 numOfProposals; // Total
+    // uint256 votePrice = 100000000000000000; // In WEI, 0.1ETH
 
     // Contract core data
     struct InitiativeProposal {
@@ -28,8 +34,7 @@ contract IzanagiDAO is ReentrancyGuard, AccessControl {
 
     mapping(uint256 => InitiativeProposal) private initiativeProposals;
     mapping(address => uint256[]) private stakeholderVotes;
-    mapping(address => uint256) private contributors;
-    mapping(address => uint256) private stakeholders;
+    mapping(address => uint256) private shares;
 
     // Contract events
     event ContributionReceived(address indexed fromAddress, uint256 amount);
@@ -39,15 +44,25 @@ contract IzanagiDAO is ReentrancyGuard, AccessControl {
         address indexed initiativeAddress,
         uint256 amount
     );
+    event BuyShares(address _buyer, uint256 _amountOfMATIC);
 
     // Contract modifiers
     modifier onlyStakeholder(string memory message) {
+        (bool checked) = checkStakeholderStatus(msg.sender);
+        require(checked, "Failed to check account status");
         require(hasRole(STAKEHOLDER_ROLE, msg.sender), message);
         _;
     }
 
     modifier onlyContributor(string memory message) {
+        (bool checked) = checkStakeholderStatus(msg.sender);
+        require(checked, "Failed to check account status");
         require(hasRole(CONTRIBUTOR_ROLE, msg.sender), message);
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not a contract owner");
         _;
     }
 
@@ -70,8 +85,11 @@ contract IzanagiDAO is ReentrancyGuard, AccessControl {
 
         votable(initiativeProposal);
 
-        if (supportProposal) initiativeProposal.votesFor++;
-        else initiativeProposal.votesAgainst++;
+        if (supportProposal) {
+            initiativeProposal.votesFor++;
+        } else {
+            initiativeProposal.votesAgainst++;
+        }
 
         stakeholderVotes[msg.sender].push(initiativeProposal.id);
     }
@@ -92,16 +110,24 @@ contract IzanagiDAO is ReentrancyGuard, AccessControl {
         }
     }
 
-    function payInitiative(uint256 proposalId) external onlyStakeholder("Only stakeholders are allowed to make payments") {
-        InitiativeProposal storage initiativeProposal = initiativeProposals[proposalId];
+    function payInitiative(uint256 _proposalId) external onlyStakeholder("Only stakeholders are allowed to make payments") {
+        InitiativeProposal storage initiativeProposal = initiativeProposals[_proposalId];
 
-        if (initiativeProposal.paid)
+        if (initiativeProposal.votingPassed || initiativeProposal.livePeriod <= block.timestamp) {
+            initiativeProposal.votingPassed = true;
+        } else {
+            revert("Voting period is not finished yet");
+        }
+
+        if (initiativeProposal.paid) {
             revert("Payment has been made to this initiative");
+        }
 
-        if (initiativeProposal.votesFor <= initiativeProposal.votesAgainst)
+        if (initiativeProposal.votesFor <= initiativeProposal.votesAgainst) {
             revert(
                 "The proposal does not have the required amount of votes to pass"
             );
+        }
 
         initiativeProposal.paid = true;
         initiativeProposal.paidBy = msg.sender;
@@ -111,31 +137,58 @@ contract IzanagiDAO is ReentrancyGuard, AccessControl {
         return initiativeProposal.initiativeAddress.transfer(initiativeProposal.amount);
     }
 
-    receive() external payable {
-        emit ContributionReceived(msg.sender, msg.value);
+    function cancelInitiative(uint256 _proposalId) external {
+        InitiativeProposal storage initiativeProposal = initiativeProposals[_proposalId];
+
+        if (initiativeProposal.votingPassed || initiativeProposal.livePeriod <= block.timestamp) {
+            initiativeProposal.votingPassed = true;
+        } else {
+            revert("Voting period is not finished yet");
+        }
+
+        if (initiativeProposal.paid) {
+            revert("Payment has been made to this initiative");
+        }
+
+        if (initiativeProposal.proposer != msg.sender) {
+            revert("Not owner of a proposal");
+        }
+
+        initiativeProposal.paid = true;
+        initiativeProposal.paidBy = msg.sender;
+
+        emit PaymentTransfered(msg.sender, initiativeProposal.proposer, initiativeProposal.amount);
+        return initiativeProposal.proposer.transfer(initiativeProposal.amount);
     }
 
-// TODO: This should be checked by selection of msg sender amount
-    function makeStakeholder(uint256 amount) external {
-        address account = msg.sender;
-        uint256 amountContributed = amount;
+    function checkStakeholderStatus(address _address) private returns (bool success) {
+        uint256 totalContributed = shares[_address];
 
-        if (!hasRole(STAKEHOLDER_ROLE, account)) {
-            uint256 totalContributed =
-                contributors[account] + amountContributed;
+        if (!hasRole(STAKEHOLDER_ROLE, _address)) {
+
             if (totalContributed >= 5 ether) {
-                stakeholders[account] = totalContributed;
-                contributors[account] += amountContributed;
-                _setupRole(STAKEHOLDER_ROLE, account);
-                _setupRole(CONTRIBUTOR_ROLE, account);
+                _setupRole(STAKEHOLDER_ROLE, _address);
+                _setupRole(CONTRIBUTOR_ROLE, _address);
             } else {
-                contributors[account] += amountContributed;
-                _setupRole(CONTRIBUTOR_ROLE, account);
+                _setupRole(CONTRIBUTOR_ROLE, _address);
             }
-        } else {
-            contributors[account] += amountContributed;
-            stakeholders[account] += amountContributed;
         }
+
+        return true;
+    }
+
+    function buyShares() external payable returns (bool success) {
+        require(msg.value > 0, "Not enough MATIC on account balance or MATIC not sent.");
+        shares[msg.sender] += msg.value;
+        emit BuyShares(msg.sender, msg.value);
+        return true;
+    }
+
+    function ownerWithdrawShares() external onlyOwner {
+        uint256 ownerBalance = address(this).balance;
+        require(ownerBalance > 0, "No MATIC present in Vendor");
+        (bool sent,) = msg.sender.call{value: address(this).balance}("");
+        require(sent, "Failed to withdraw");
     }
 
     function getProposals() public view returns (InitiativeProposal[] memory props) {
@@ -154,19 +207,15 @@ contract IzanagiDAO is ReentrancyGuard, AccessControl {
         return stakeholderVotes[msg.sender];
     }
 
-    function getStakeholderBalance() public view onlyStakeholder("User is not a stakeholder") returns (uint256) {
-        return stakeholders[msg.sender];
+    function getBalance() public view returns (uint256) {
+        return shares[msg.sender];
     }
 
     function isStakeholder() public view returns (bool) {
         return stakeholders[msg.sender] > 0;
     }
 
-    function getContributorBalance() public view onlyContributor("User is not a contributor") returns (uint256) {
-        return contributors[msg.sender];
-    }
-
     function isContributor() public view returns (bool) {
-        return contributors[msg.sender] > 0;
+        return shares[msg.sender] > 0;
     }
 }
