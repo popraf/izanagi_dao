a23ea2b58812a58e0542c9e5724950b24e720fdc
Rafał <rafal44334@gmail.com>
2023-07-03 16:38:37 +0200

    Frontend: Profile in progress

--- izanagi_frontend/pages/profile.js
+++ izanagi_frontend/pages/profile.js
@@ -1,87 +1,91 @@
 import BaseLayout from "../components/BaseLayout";
 import withAuth from "../utils/withAuth";
-import styles from "../styles/Home.module.css";
+import ProfileStyles from "../styles/Profile.module.css";
 
 const Profile = () => {
   return(
     <BaseLayout>
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Profile
-        </h1>
+    {/* <div className={styles.container}> */}
 
-        <p className={styles.description}>
-          Members of Izanagi DAO have the opportunity to propose projects that aim to create positive change in society.
-          <br/>
-          Learn more about decentralized nature of the project, enabling
-          individuals to work towards common social objectives.
-        </p>
+      <main className={ProfileStyles.main}>
+        <h1 className={ProfileStyles.title}>Profile</h1>
 
-        <h2>Details</h2>
-        <div className={styles.grid_text}>
-            Izanagi DAO focuses on social impact initiatives by providing a platform for 
-            its members to propose and support projects that address various environmental, 
-            social, or governance challenges. The DAO (Decentralized Autonomous Organization)
-            model allows for decentralized decision-making and resource allocation.
+        <h2 className={ProfileStyles.subtitle}>Contribution</h2>
+          <div className={ProfileStyles.grid}>
+            {/* <a className={ProfileStyles.card}> */}
+              <h2>Account Status</h2>
+              <p>Contributor</p>
+            {/* </a> */}
+          </div>
 
-            Members of Izanagi DAO have the opportunity to propose projects that aim to
-            create positive change in society. These projects can span a wide range of areas,
-            such as environmental conservation, social equality, community development,
-            education, healthcare, and more. The DAO fosters a collaborative and participatory
-            environment where members can contribute their ideas and expertise to drive
-            meaningful impact.
+        <h2 className={ProfileStyles.subtitle}>Account Details</h2>
+          <div className={ProfileStyles.grid}>
+            <a className={ProfileStyles.card}>
+              <h2>Account Status</h2>
+              <p>Contributor</p>
+            </a>
 
-            Once a project proposal is submitted, the members of Izanagi DAO have the ability
-            to support and vote on the project's funding allocation. This democratic process
-            ensures that projects align with the collective vision and goals of the DAO's
-            community. Funding can be allocated to support sustainable development initiatives,
-            charitable causes, or community-driven projects that aim to uplift marginalized
-            communities.
+            <a className={ProfileStyles.card}>
+              <h2>Balance</h2>
+              <p>123 MATIC</p>
+            </a>
 
-            By leveraging blockchain technology and smart contracts, Izanagi DAO 
-            provides transparency and accountability in project funding and execution. 
-            The decentralized nature of the DAO enables individuals from diverse backgrounds 
-            to come together and work towards common social objectives.
+            <a className={ProfileStyles.card}>
+              <h2>Total Votes</h2>
+              <p>Account is not stakeholder.</p>
+              <p> Contribute at least 5 MATIC to become one.</p>
+            </a>
+          </div>
 
-            Through its focus on social impact initiatives, Izanagi DAO strives to create a
-            positive and sustainable future, where individuals and communities can thrive. 
-            By empowering its members to propose and support projects that address pressing 
-            societal challenges, Izanagi DAO aims to make a tangible difference and 
-            contribute to the betterment of society as a whole.
-        </div>
+        <h2 className={ProfileStyles.subtitle}>Proposals Submitted</h2>
+          <div className={ProfileStyles.grid}>
+            <a className={ProfileStyles.card}>
+              <h2>Proposal 1</h2>
+              <p>Proposal 1 Description</p>
+            </a>
 
-      <h2>Proposals</h2>
-        <div className={styles.grid_text}>
-            The "Proposals" feature of Izanagi DAO allows members to submit their project
-            ideas and initiatives for consideration and funding. It serves as a collaborative
-            platform where individuals can present innovative proposals that align with the
-            DAO's mission of driving positive social impact.
+            <a className={ProfileStyles.card}>
+              <h2>Proposal 2</h2>
+              <p>Proposal 2 Description</p>
+            </a>
 
-            Members are encouraged to submit proposals that address environmental, social, or
-            governance challenges and have the potential to create meaningful change.
-            These proposals can cover a wide range of areas, such as sustainable development,
-            community empowerment, social equality, education, healthcare, and more.
+            <a className={ProfileStyles.card}>
+              <h2>Proposal 3</h2>
+              <p>Proposal 3 Description</p>
+            </a>
+          </div>
 
-            Once a proposal is submitted, it undergoes a transparent evaluation process within
-            the DAO community. Members have the opportunity to review and provide feedback on
-            the proposal, fostering an environment of collaboration and constructive dialogue.
+        <h2 className={ProfileStyles.subtitle}>Manage Submitted Proposals</h2>
+          <div className={ProfileStyles.grid}>
+            <a className={ProfileStyles.card}>
+              <h2>Withdraw</h2>
+              <p>Proposal 1 Description</p>
+            </a>
 
-            The proposals that receive sufficient support and consensus from the DAO community
-            are then eligible for funding. The DAO members vote to allocate resources and 
-            financial support to the selected projects, enabling them to come to fruition.
+            <a className={ProfileStyles.card}>
+              <h2>Proposal 3</h2>
+              <p>Proposal 3 Description</p>
+            </a>
+          </div>
 
-            Through its "Proposals" feature, Izanagi DAO empowers its members to contribute
-            their ideas and expertise, fostering a collective effort to address pressing
-            societal challenges. It exemplifies the DAO's commitment to promoting social
-            innovation and creating a positive impact on communities and the world at large.
-        </div>
+        <h3>User Details</h3>
+          <div>
+            <h5>known statuses, functions</h5>
+            <p>Status/is stakeholder, is contributor</p>
+            <p>Total contributed</p>
+            <p>Become stakeholder</p>
+            <p>Get stakeholder votes</p>
+            <p>Get stakeholder balance, get contributor balance</p>
+            <p>User proposals</p>
+          </div>
 
       </main>
-    </div>
+
+    {/* </div> */}
 
   </BaseLayout>
   );
 };
 
-export default withAuth(Profile);
+// export default withAuth(Profile);
+export default (Profile);
