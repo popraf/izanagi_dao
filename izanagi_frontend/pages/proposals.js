813a0860ddbdc43df6c7bddc1f2970773ce35ec7
Rafał <rafal44334@gmail.com>
2023-07-04 13:09:51 +0200

    Frontend: Proposals table init

--- izanagi_frontend/pages/proposals.js
+++ izanagi_frontend/pages/proposals.js
@@ -1,15 +1,60 @@
 import BaseLayout from "../components/BaseLayout";
+import { useState } from "react";
+import Loader from "../components/Loader";
+import NewProposalStyles from "../styles/NewProposal.module.css";
+import styles from "../styles/Home.module.css";
+import ProposalsStyles from "../styles/Proposals.module.css";
 
 const Proposals = () => {
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleFormFieldChange = (fieldName, e) => {
+    setForm({ ...form, [fieldName]: e.target.value })
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  }
+
   return(
-  <BaseLayout>
-    <h1>Proposals</h1>
-    <div>
-    Focuses on social impact initiatives.
-    Members can propose and support projects that address environmental, social, or governance challenges.
-    Funding can be allocated to support sustainable development, charitable causes, or community-driven initiatives.
-    </div>
-  </BaseLayout>
+    <BaseLayout>
+      <div className={ProposalsStyles.main}>
+        <h1 className={ProposalsStyles.title}>Search through <a>the proposals</a></h1>
+
+<table className={ProposalsStyles.table__container}>
+        {/* <table className={ProposalsStyles.table}> */}
+{/* <div className={ProposalsStyles.table__header}> */}
+          <thead className={ProposalsStyles.table__header}>
+            <tr className={ProposalsStyles.table__tr}>
+              <th> Id</th>
+              <th> Voting End Date </th>
+              <th> Description </th>
+              <th> Amount </th>
+              <th> Proposed Address </th>
+              <th> Initiative Address </th>
+              <th> Upvotes </th>
+              <th> Downvotes </th>
+            </tr>
+          </thead>
+{/* </div> */}
+          <tbody className={ProposalsStyles.tbody}>
+            <tr className={ProposalsStyles.table__tr}>
+              <td> 1 </td>
+              <td> 17 Dec, 2022 </td>
+              <td> This initiative... </td>
+              <td> 200MATIC </td>
+              <td> 0x0000 </td>
+              <td> 0x1000 </td>
+              <td> 20 </td>
+              <td> 5 </td>
+            </tr>
+          </tbody>
+
+        </table>
+{/* </div> */}
+
+      </div>
+    </BaseLayout>
   );
 };
 
